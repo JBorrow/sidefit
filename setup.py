b946e4235fe4298549dcb2e465fca252088cff8a
Josh Borrow <joshua.borrow@durham.ac.uk>
2018-03-13 08:11:41 +0000

    Added packaging requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="sidefit",
+    version="0.1",
+    description="A radial chi-square fitter",
+    url="https://github.com/jborrow/sidefit",
+    author="Josh Borrow",
+    author_email="joshua.borrow@durham.ac.uk",
+    packages=["sidefit"],
+    zip_safe=False
+)
